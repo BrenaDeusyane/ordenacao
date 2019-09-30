c77f77a964fbdc74c378f9cacbfb8e8dc2528c41
Brena Deusyane <deusyane.1000@gmail.com>
2019-09-30 20:12:03 -0300

    Organizando o vetor em ordem crescente em duas partes

--- MergeSort.c
+++ MergeSort.c
@@ -25,5 +25,35 @@ int main(void) {
     }
   }
 
+//Dividi o vetor em duas partes
+  for (int i=0; i<=quantidade/2; i++){
+    vetor_auxiliar1[i] = vetor_principal[(quantidade/2)+i];
+  }
+
+//Organizando os valores em ordem crescente do vetor principal(Os primeiros valores do vetor)
+  int j;
+  for (int i=0; i < quantidade/2; i++){
+    for(j = i+1; j<quantidade/2; j++){
+      if (vetor_principal[i] > vetor_principal[j]){
+        vetor_auxiliar2[j-1] = vetor_principal[j];
+      }
+      else{
+        vetor_auxiliar2[j-1] = vetor_principal[j];
+      }
+    }
+  }
+
+//Organizando os valores em ordem crescente do vetor auxiliar1(Os segundos valores do vetor)
+  for (int i=0; i < quantidade/2; i++){
+    for(j = j+1; j<quantidade/2; j++){
+      if (vetor_auxiliar1[i] > vetor_auxiliar1[i+1]){
+        vetor_auxiliar2[j] = vetor_auxiliar1[i+1];
+      }
+      else{
+        vetor_auxiliar2[j] = vetor_auxiliar1[i];
+      }
+    }
+  }
+
   return 0;
 }
