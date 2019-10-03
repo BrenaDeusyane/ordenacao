65482e0c574d75521d6e72bfd72effb764060c3a
Brena Deusyane <deusyane.1000@gmail.com>
2019-10-03 00:00:01 -0300

    Correção de linhas no lugar errado e verificando se o vetor rodou até o meio

--- MergeSort.c
+++ MergeSort.c
@@ -1,39 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void){
   int tam_vetor;
-  int vetor[tam_vetor];
-  int inicio = 0, meio, fim = tam_vetor-1;
-  
+
   printf("\nQuantos valores serão utilizados?\n");
   scanf("%d", &tam_vetor);
 
+  int vetor[tam_vetor];
+  int vetor_aux[tam_vetor];
+  int inicio = 0, meio, fim = tam_vetor-1;
+  int com1 = inicio, com2, aux = 0;
+  
   if(inicio < fim){
     meio = tam_vetor/2;
-    int com1 = inicio, com2 = meio+1, aux = 0;
-    int vetor_aux[tam_vetor];
-  }
-  else{
+    com2 = meio+1;
+  }else{
     printf("*********** Não há quantidade suficiente! ***********\n");
-    exit 0;
+    printf("*********** BYE ***********\n\n");
+    exit(0);
   }
 
 //Guardando os dados do vetor
   for (int i=0; i < tam_vetor; i++){
-    printf("Insira o %d° valor inteiro:\n", i+1);
-    scanf("%d", vetor[i]);
+    printf("\nInsira o %d° valor inteiro:\n", i+1);
+    scanf("%d", &vetor[i]);
   }
 
 //Comparando o inicio do vetor, com meio do vetor em diante e colocando no vetor auxiliar
-  while(com1 <= meio && com2 <= fim){
+  while(  (com1 <= meio) && (com2 <= fim) ){
     if (vetor[com1] < vetor[com2]){
       vetor_aux[aux] = vetor[com1];
       com1++;
-    }
-    else{
+    }else{
       vetor_aux[aux] = vetor[com2];
+      com2++;
     }
     aux++;
   }
+//Caso a primeira parte do vetor não tenha sido rodada inteira
+  for(int i = com1; i <= meio; i++){
+    vetor_aux[aux] = vetor[i];
+    aux++;
+  }
+
+  printf("\n[");
+  for(int i=0; i<tam_vetor; i++){
+    printf("%d ", vetor_aux[i]);
+  }
+  printf("]\n");
+
+  return 0;
 
 }
