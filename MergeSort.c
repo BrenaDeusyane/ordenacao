7a94fcf3e48c39e073f05bdcc4b81c21ba40c0d3
Brena Deusyane <deusyane.1000@gmail.com>
2019-10-02 23:36:07 -0300

    Reiniciei o código e começei a verificar os menosres valores

--- MergeSort.c
+++ MergeSort.c
@@ -1,77 +1,39 @@
 #include <stdio.h>
 
-int main(void) {
-  int quantidade;
+int main(void){
+  int tam_vetor;
+  int vetor[tam_vetor];
+  int inicio = 0, meio, fim = tam_vetor-1;
+  
+  printf("\nQuantos valores serão utilizados?\n");
+  scanf("%d", &tam_vetor);
 
-  printf("Qual a quantidade?\n");
-  scanf("%d", &quantidade);
-
-  int vetor_principal[quantidade-1];
-  int vetor_auxiliar1[quantidade-1];
-  int vetor_auxiliar2[quantidade-1];
-  int aux;
-
-  for(int i=0; i<quantidade; i++){
-    scanf("%d", &vetor_principal[i]);
+  if(inicio < fim){
+    meio = tam_vetor/2;
+    int com1 = inicio, com2 = meio+1, aux = 0;
+    int vetor_aux[tam_vetor];
   }
-// Colocando o menor valor do par, nos números pares
-for(int i=0; i<quantidade-1; i=i+2){
-    if(vetor_principal[i] > vetor_principal[i+1]){
-      aux = vetor_principal[i];
-      vetor_principal[i] = vetor_principal[i+1];
-      vetor_principal[i+1] = aux;
+  else{
+    printf("*********** Não há quantidade suficiente! ***********\n");
+    exit 0;
+  }
+
+//Guardando os dados do vetor
+  for (int i=0; i < tam_vetor; i++){
+    printf("Insira o %d° valor inteiro:\n", i+1);
+    scanf("%d", vetor[i]);
+  }
+
+//Comparando o inicio do vetor, com meio do vetor em diante e colocando no vetor auxiliar
+  while(com1 <= meio && com2 <= fim){
+    if (vetor[com1] < vetor[com2]){
+      vetor_aux[aux] = vetor[com1];
+      com1++;
     }
-  }
-
-//Dividi o vetor em duas partes
-  for (int i=0; i<=quantidade/2; i++){
-    vetor_auxiliar1[i] = vetor_principal[(quantidade/2)+i];
-  }
-
-//Organizando os valores em ordem crescente do vetor principal(Os primeiros valores do vetor)
-  int j;
-  for (int i=0; i < quantidade/2; i++){
-    for(j = i+1; j<quantidade/2; j++){
-      if (vetor_principal[i] > vetor_principal[j]){
-        vetor_auxiliar2[j-1] = vetor_principal[j];
-      }
-      else{
-        vetor_auxiliar2[j-1] = vetor_principal[j];
-      }
+    else{
+      vetor_aux[aux] = vetor[com2];
     }
+    aux++;
   }
 
-//Organizando os valores em ordem crescente do vetor auxiliar1(Os segundos valores do vetor)
-  for (int i=0; i < quantidade/2; i++){
-    for(j = j+1; j<quantidade/2; j++){
-      if (vetor_auxiliar1[i] > vetor_auxiliar1[i+1]){
-        vetor_auxiliar2[j] = vetor_auxiliar1[i+1];
-      }
-      else{
-        vetor_auxiliar2[j] = vetor_auxiliar1[i];
-      }
-    }
-  }
-
-//Comparando os valores que foram divididos e agrupados, colocando em ordem crescente, passo final
-  int k = (quantidade/2) + 1;
-  for (int i=0; i!=k; i++){
-    for (k; k <=quantidade; k++){
-      if (vetor_auxiliar2[i] > vetor_auxiliar2[k]){
-        vetor_principal[i] = vetor_auxiliar2[k];
-      }
-      else{
-        vetor_principal[i] = vetor_auxiliar2[i];
-      }
-    }
-  }
-
-//Imprimindo
-  printf("\n[");
-  for (int i=0; i<quantidade; i++){
-    printf("%d ", vetor_principal[i]);
-  }
-  printf("]\n");
-
-  return 0;
 }
